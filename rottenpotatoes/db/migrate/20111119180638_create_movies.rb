90fa084b65dc6a0ec1050be4ba4879e0607a7303
pjy4183 <pjy4183@gmail.com>
2021-10-14 16:05:21 +0000

    part1

--- rottenpotatoes/db/migrate/20111119180638_create_movies.rb
+++ rottenpotatoes/db/migrate/20111119180638_create_movies.rb
@@ -2,6 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def up
     create_table :movies do |t|
       t.string :title
+      t.string :director 
       t.string :rating
       t.text :description
       t.datetime :release_date
